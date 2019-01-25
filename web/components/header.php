6dd24e6de7e7f0b2653a9c2cb6598dc3172c13a1
Milan <miliup@live.com>
2019-01-25 21:56:29 +0100

    add components for sass

--- web/components/header.php
+++ web/components/header.php
@@ -1,7 +1,77 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: mili
- * Date: 1/25/2019
- * Time: 9:11 AM
- */
+<section class="container">
+    <div class="header">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="col-md-4">
+                        <h1 class="mala">Wapik</h1>
+                    </div>
+                    <div class="col-md-1"></div>
+                    <div class="col-md-7">
+                        <div class="row">
+                            <div class="phone">
+                                <i class="fa fa-phone fa-2x" aria-hidden="true" ></i>
+                            </div>
+                            <div class="number">
+                                <p>+62 202 55 0117</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="col-md-9">
+                    <div class="menu">
+                        <ul>
+                            <li>Home</li>
+                            <li>About Us</li>
+                            <li>Pricing</li>
+                            <li>Contact Us</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <button>Get Started</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--
+<div class="row header">
+    <div class="col-md-6 col-sm-7 col-6">
+        <div class="row justify-content-between">
+            <div class="col-md-2 col-sm-4">
+                <h1 class="logo h1_size"><a class="white" href="index.html">Wapik</a></h1>
+            </div>
+            <div class="col-md-1 col-sm-1 line">
+            </div>
+            <div class="col-md-7 col-sm-6 navigation phone_h">
+                <div class="row">
+                    <div class="phone">
+                        <i class="fa fa-phone fa-2x" aria-hidden="true" ></i>
+                    </div>
+                    <div class="number">
+                        <p>+62 202 555 0117</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-6 col-sm-5 col-6 row no_padding">
+        <div class="col-lg-9 col-md-12 no_padding">
+            <div class="menu">
+                <ul class="nav navigation justify-content-end">
+                    <li class="menuPosition"><a class="grayNav" href="index.html">home</a></li>
+                    <li class="menuPosition"><a class="grayNav" href="AboutUs.html">about us</a></li>
+                    <li class="menuPosition"><a class="grayNav" href="Pricing.html">pricing</a></li>
+                    <li><a class="grayNav" href="ContactUs.html">Contact us</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-md-3 no_padding">
+            <button class="buttonGp buttonG buttonImplies">Get Started</button>
+        </div>
+        <button class="menu-btn"><i class="fa fa-bars fa-2x" aria-hidden="true"></i></button>
+    </div>
+</div> -->
--- web/index.php
+++ web/index.php
@@ -6,7 +6,6 @@
     <link rel="stylesheet" href="stylesheets/style.css" />
 </head>
 <body>
-<h1>Naslov</h1>
 
 <script src="javascript/main.min.js"></script>
 </body>
