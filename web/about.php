f9cc812987986967297920d1266b57e0337ce483
Milan <miliup@live.com>
2019-01-25 09:54:23 +0100

    preparing a project for sass

---
+++ web/about.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="stylesheets/style.css" />
+</head>
+<body>
+<h1>Naslov</h1>
+
+<script src="javascript/main.min.js"></script>
+</body>
+</html>
---
+++ web/components/footer.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mili
+ * Date: 1/25/2019
+ * Time: 9:11 AM
+ */
