7238c1367df4486249085f5b15453449e0a26120
Milan <miliup@live.com>
2019-02-13 02:44:12 +0100

    resolve feature/2-home-page

--- web/components/include-check.php
+++ web/components/include-check.php
@@ -1,7 +1,57 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: mili
- * Date: 2/13/2019
- * Time: 1:10 AM
- */
+<div class="include-check">
+    <div class="features__head">
+        <h3>Include with all Plans</h3>
+        <hr class="hr_all">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="row plan-check">
+                    <img class="check__img" src="img/home/vector_s_o.png">
+                    <p class="check__p">Maecenas faucibus mollis intedum</p>
+                </div>
+                <div class="row plan-check">
+                    <img class="check__img" src="img/home/vector_s_o.png">
+                    <p class="check__p">Cras justo odio, dapibus ac facilisis</p>
+                </div>
+                <div class="row plan-check">
+                    <img class="check__img" src="img/home/vector_s_o.png">
+                    <p class="check__p">Fringilla Mattis</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="row plan-check">
+                    <img class="check__img" src="img/home/vector_s_o.png">
+                    <p class="check__p">Vestibulum id ligula porta felis euismod</p>
+                </div>
+                <div class="row plan-check">
+                    <img class="check__img" src="img/home/vector_s_o.png">
+                    <p class="check__p">Duis mollis, est non commodo luctus</p>
+                </div>
+                <div class="row plan-check">
+                    <img class="check__img" src="img/home/vector_s_o.png">
+                    <p class="check__p">Integer posuere</p>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="row plan-check">
+                    <img class="check__img" src="img/home/vector_s_o.png">
+                    <p class="check__p">Nulla vitae elit libero, a pharetra augue</p>
+                </div>
+                <div class="row plan-check">
+                    <img class="check__img" src="img/home/vector_s_o.png">
+                    <p class="check__p">Curabitur blandit tempus porttior</p>
+                </div>
+                <div class="row plan-check">
+                    <img class="check__img" src="img/home/vector_s_o.png">
+                    <p class="check__p">Nullam quis risus eget urna mollis</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="features__head">
+        <img  class="img_help" src="img/home/help.png">
+        <h1>Need Help?</h1>
+        <hr class="hr_all">
+        <p class="p-for-all help-p">Contact our Customer Support that is always ready to help you with any possible<br>questions, problems or information.</p>
+        <h5>support@wapik.com</h5>
+    </div>
+</div>
--- web/index.php
+++ web/index.php
@@ -7,9 +7,161 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-    <section class="section_home_1">
-        <?php include "components/header.php"?>
-
+    <section class="section_home_1 container-fluid">
+        <div class="container">
+            <?php include "components/header.php"?>
+            <div class="head-line">
+                <h1 class="h1_size">Welcome to Wapik</h1>
+                <p class="p_head">Wapik is multipurpose template, with modern and smart desing. Wapik<br> is prefect templete for you!</p>
+                <button class="head-line__button">Learn More</button>
+            </div>
+        </div>
+    </section>
+    <section class="section_home_2">
+        <div class="container">
+            <div class="features">
+                <div class="features__head">
+                    <h1>Beset of our features</h1>
+                    <hr class="hr_all">
+                    <p class="p-for-all">Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br> Integer posuere erat a ante venenatis dapibus posuere velit aliquet.</p>
+                </div>
+                <div class="feature__colum">
+                    <div class="row">
+                        <div class="col-md-4 col-sm-4 col-4">
+                            <img src="img/home/icon/vector.png">
+                            <h3 class="feature__heading">lonicons</h3>
+                            <p class="p-for-all">Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Aenean eu leo quam. Pellentesque ornare sem.</p>
+                        </div>
+                        <div class="col-md-4 col-sm-4 col-4">
+                            <img src="img/home/icon/vector_1.png">
+                            <h3 class="feature__heading">Creative Design</h3>
+                            <p class="p-for-all">Maecenas faucibus mollis interdum. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
+                        </div>
+                        <div class="col-md-4 col-sm-4 col-4">
+                            <img class="feature__img" src="img/home/icon/vector_2.png">
+                            <h3 class="feature__heading">Photoshop and Sketch</h3>
+                            <p class="p-for-all">Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Maecenas sed diam eget risus varius blandit sit amet non magna.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="feature__te">
+                    <div class="row justify-content-center">
+                        <div class="div0 div1"></div>
+                        <div class="div0 div2"></div>
+                    </div>
+                </div>
+                <div class="feature__colum-other">
+                    <div class="row">
+                        <div class="col-md-6 col-sm-6">
+                            <div class="row">
+                                <img class="img_colum" src="img/home/icon_o/vector_o_1.png">
+                                <h3>Cross Browser Compatibility</h3>
+                            </div>
+                            <p class="p-for-all">Donec ullamcorper nulla non metus auctor fringilla. Donec id elit non mi porta gravida at eget metus.</p>
+                        </div>
+                        <div class="col-md-6 col-sm-6">
+                            <div class="row">
+                                <img class="img_colum" src="img/home/icon_o/vector_o_3.png">
+                                <h3>Creative Idea</h3>
+                            </div>
+                            <p class="p-for-all">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum id ligula portavfelis euismod semper.</p>
+                        </div>
+                        <div class="col-md-6 col-sm-6">
+                            <div class="row">
+                                <img class="img_colum" src="img/home/icon_o/vector_o_2.png">
+                                <h3>Pixel Perfect</h3>
+                            </div>
+                            <p class="p-for-all">Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        </div>
+                        <div class="col-md-6 col-sm-6">
+                            <div class="row">
+                                <img class="img_colum" src="img/home/icon_o/vector_o_4.png">
+                                <h3>Free Forever and Ever</h3>
+                            </div>
+                            <p class="p-for-all">Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida at eget metus.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section_home_3 container-fluid">
+        <div class="container">
+            <div class="features__head">
+                <h1>Looking for the perfect template to use?</h1>
+                <hr class="hr_all">
+                <p class="p-for-all">Donec id elit non mi porta gravida at eget metus.</p>
+            </div>
+            <div class="row">
+                <div class="col-md-6 col-sm-6 col-6">
+                    <img class="hand" src="img/home/Hand.png">
+                </div>
+                <div class="col-md-6 col-sm-6 col-6">
+                    <h2>Responsive Ready</h2>
+                    <p class="p-for-all">Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut<br>fermentum massa justo sit amet risus. Praesent commodo cursus magna, vel<br>scelerisque nisl consectetur et. Aenean lacinia bibendum nulla sed<br>consectetur. Maecenas sed diam eget risus varius blandit sit amet non magna.<br>Vestibulum id ligula porta felis euismod semper.</p>
+                    <div class="check">
+                        <div class="row">
+                            <img class="check__img" src="img/home/vector_s_o.png">
+                            <p class="check__p">Integer posuere erat a ante venenatis dapibus posuere velit.</p>
+                        </div>
+                    </div>
+                    <div class="check">
+                        <div class="row">
+                            <img class="check__img" src="img/home/vector_s_o.png">
+                            <p class="check__p">Donec id elit non mi porta gravida at eget metus.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section_home_4">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-sm-6 col-6">
+                    <p class="home-4__p-theme">This theme is beautiful, elegant and easy to<br>customize. I’ve been able to build an amazing<br>site in just a minute. Thank you very much for<br>creating this impressive template!</p>
+                    <p class="home-4__p-valeria">Valeria Boltneva</p>
+                    <p class="home-4__p-acme">Director of Acme.Inc</p>
+                </div>
+                <div class="col-md-6 col-sm-6 col-6">
+                    <img class="imgR" src="img/home/valeria.png">
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section_home_5 container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-sm-6 col-6">
+                    <div class="built">
+                        <h2>Built with High Attention to Details</h2>
+                        <p class="p-for-all">Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut<br>fermentum massa justo sit amet risus. Praesent commodo cursus magna, vel<br>scelerisque nisl consectetur et. Aenean lacinia bibendum nulla sed<br>consectetur. Maecenas sed diam eget risus varius blandit sit amet non magna.<br>Vestibulum id ligula porta felis euismod semper. Integer posuere erat a ante<br>venenatis dapibus posuere velit aliquet. Donec id elit non mi porta gravida at eget metus.</p>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-6 col-6 planet">
+                    <div class="planet">
+                        <img class="image-planet" src="img/home/planet.png">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="section-home-6">
+        <div class="container">
+            <div class="features__head plan-pricing">
+                <h1 class="h_color h1_size">Parcing & Plan</h1>
+                <hr class="hr_all">
+                <p class="p-for-all">Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.<br>Donec id elit non mi porta gravida at eget metus.</p>
+            </div>
+            <?php include "components/pricing.php"?>
+            <?php include "components/include-check.php"?>
+        </div>
+    </section>
+    <section class="section-home-7 container-fluid">
+        <?php include "components/download.php"?>
+    </section>
+    <section class="section-home-8">
+        <?php include "components/footer.php"?>
     </section>
 <script src="javascript/main.min.js"></script>
 </body>
